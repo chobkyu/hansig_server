0366e5776c78bcc1abaf62a182219d997d9caa31
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-12-06 17:01:42 +0900

    update

---
+++ test/user.spec.ts
@@ -0,0 +1,31 @@
+const app = require('../app')
+const request = require('supertest')
+
+describe('post /user is..', function () {
+    describe('success', () => {
+        let body = {
+            name: 'test',
+            age: 5
+        }
+        it('201으로 응답한다', (done) => {
+            request(app)
+                .post('/users')
+                .send(body)
+                .expect(201)
+                .end(done)
+        }); 
+
+    });
+    describe('fail..',()=>{
+        it('입력이 잘못되었을 경우 400으로 응답한다',(done)=>{
+            let body = {
+                name: 'test',
+            }
+            request(app)
+                .post('/users')
+                .send(body)
+                .expect(400)
+                .end(done)
+        })
+    })
+})
