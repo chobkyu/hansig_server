33c449ea9d9e096c7ebf7f183d547cb9a224367a
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-12-06 11:15:02 +0900

    test code

--- src/router/users/user.ctrl.ts
+++ src/router/users/user.ctrl.ts
@@ -9,7 +9,11 @@ const process = {
             console.log(req.body)
             const userservice = new UserService(req.body)
             const response = await userservice.insertUser();
-    
+            
+            if(req.body.name == null || req.body.age == null){
+                return res.status(400).json({err:'데이터 없음'})
+            }
+            console.log(response.success)
             if(response.success){
                 return res.status(201).end();
             }
--- test/hansic.spec.js
+++ test/hansic.spec.js
@@ -4,14 +4,14 @@ const request = require('supertest')
 describe('GET /hansic is ...', function () {
     this.timeout(10000)
     describe('success', async () => {
-        it('return data', async done => {
+        it('return data', async () => {
             request(app)
                 .get('/hansic')
                 .end(async (err, res) => {
 
-                    console.log(res.body)
-                    await res.body.should.be.instanceOf(Array)
-                    done();
+                    //console.log(res.body)
+                    res.body.should.be.instanceOf(Array)
+                    //done();
 
 
                 })
@@ -19,8 +19,19 @@ describe('GET /hansic is ...', function () {
     })
 })
 
-describe('post /user is..',function () {
-    describe('success',()=>{
-        
-    })
+describe('post /user is..', function () {
+    describe('success', () => {
+        let body = {
+            name: 'test',
+            age: 5
+        }
+        it('201으로 응답한다', (done) => {
+            request(app)
+                .post('/users')
+                .send(body)
+                .expect(201)
+                .end(done)
+        }); 
+
+    });
 })
