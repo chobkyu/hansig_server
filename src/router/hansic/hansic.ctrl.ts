5ecd063f44cd37897522c9bc922c0b4d089e3082
slugjeon <youngman0515@naver.com>
2023-12-30 00:52:53 +0900

    1230

--- src/router/hansic/hansic.ctrl.ts
+++ src/router/hansic/hansic.ctrl.ts
@@ -9,6 +9,24 @@ const output = {
         if(response.success) return res.json(response);
         else return res.status(500);
     
+    },
+    menu:
+    {
+        getAll:async (req:Request,res:Response) => {
+            const response = await hansicService.menu.getAll();
+            if(response.success) return res.json(response);
+            else return res.status(500);
+        },
+        get:async (req:Request,res:Response) => {
+            const response = await hansicService.menu.get(parseInt(req.params.id));
+            if(response.success) return res.json(response);
+            else if(res.locals.errmsg)
+            {
+                return res;
+            } 
+            else{return res.status(500);}
+            
+        }
     }
 }
 
--- src/router/hansic/index.ts
+++ src/router/hansic/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
 const ctrl = require('./hansic.ctrl')
-
+/*
+ㄴ 리뷰 입력 시
+ㄴ 리뷰 수정 시
+ㄴ 리뷰 삭제 시
+ㄴ 메뉴 입력 시
+ㄴ 메뉴 수정 시
+ㄴ 메뉴 삭제 시
+ㄴ 리뷰 댓글 입력 시
+ㄴ 리뷰 댓글 삭제 시
+*/
 const router = express.Router();
-
+//review
 router.get('/',ctrl.output.getAll);
+router.post('/:id',ctrl.process.create);
+router.patch('/:id',ctrl.process.update);
+router.delete('/:id',ctrl.process.delete);
+//menu
 
-
+//reviewcomment
 module.exports = router
